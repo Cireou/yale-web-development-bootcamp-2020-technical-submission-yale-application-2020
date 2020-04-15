84cf804d3e59eccf3d6190bbf5a3a7940ef5892e
Zi Xin Ou <eric.ou@yale.edu>
2020-04-15 02:42:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -14,7 +14,8 @@ AVERAGE_HEIGHT_IN_INCHES = 67
 name = "Eric Ou"
 age = 21
 favorite_food = "Sashimi"
-height_in_inches = ""
+height_in_inches = 70.0
+height_message = (height_in_inches > AVERAGE_HEIGHT_IN_INCHES) ? "You are taller than average." : "You are not taller than average."
 
 ## Do not modify below this line!
 
